379f9388fbcb2517ebf9d180b439ce291c638465
JamesyK <jameskanegae@gmail.com>
2015-12-21 12:34:49 -0800

    fixed ordering

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,7 +5,7 @@ class Article < ActiveRecord::Base
   has_many :bookmarks
 
   scope :order_by_newest, -> { order(created_at: :desc) }
-  scope :order_by_likes, -> { order(likes_count: :desc, created_at: :desc) }
+  scope :order_by_likes, -> { order(:likes_count, created_at: :desc) }
 
   validates :title, presence: true
   validates :url, presence: true, format: { with: /\b(https?:\/\/)([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\b/,
